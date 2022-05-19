72ceb6c17bafa2576ae05ce99559485c7b171ac2
cammac <camm@rocktechcomputer.com>
2022-05-18 20:39:32 -0400

    added image props

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
@@ -9,8 +9,15 @@ export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>
-        Halo React Native
+        Hello React Native
       </Text>
+      <Image
+        fadeDuration={500}
+        source={{
+          width: 200,
+          height: 300, 
+          uri: 'https://picsum.photos/200/300'
+        }}/>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
@@ -19,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
