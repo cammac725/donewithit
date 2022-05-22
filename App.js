36eb13fc9938ea1ff2112605b4e3f1df31c05917
cammac <camm@rocktechcomputer.com>
2022-05-22 12:34:06 -0400

    start dimensions

--- App.js
+++ App.js
@@ -1,64 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import 
   { 
+    Dimensions,
     StyleSheet, 
-    Text,
-    Alert,
-    TouchableOpacity, 
-    Button, 
-    Image,
     Platform,
+    View,
   } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
 
-  // iOS only
-  // const handlePress = () => Alert.prompt(
-  //   "Answer quickly",
-  //   "How much do you love me?",
-  //   text => Alert.alert("Tons!")
-  // )
-
-  const handlePress = () => Alert.alert(
-    "Answer if you dare",
-    "Do you love me?",
-    [
-      { text: "Yes", onPress: () => Alert.alert("Phew!") },
-      { text: "No", onPress: () => Alert.alert("What?!?!") }
-    ]
-  )
+  console.log(Dimensions.get("screen"))
 
   return (
-    <SafeAreaProvider style={[styles.container, containerStyle]}>
-      <Text>
-        Hello React Native
-      </Text>
-      <Button title="Click me" onPress={handlePress} />
-      
-      <TouchableOpacity>
-        <Image
-          fadeDuration={500}
-          source={{
-            width: 200,
-            height: 300, 
-            uri: 'https://picsum.photos/200/300'
-          }}/>
-      </TouchableOpacity>
+    <SafeAreaProvider style={styles.container}>
+      <View style={{
+        backgroundColor: 'lightblue',
+        width: '50%',
+        height: 70,
+      }}></View>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
 }
 
-const containerStyle = { backgroundColor: 'lightblue'}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    // paddingTop: 
-    //   Platform.OS === "android" ? 25 : 0,
+    paddingTop: 
+       Platform.OS === "android" ? 25 : 0,
   },
 });
