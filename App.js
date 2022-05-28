1700f97c1817b86e5ab2c71df5af8b7c8e59b30c
cammac <camm@rocktechcomputer.com>
2022-05-28 17:07:16 -0400

    add screens, border

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 // import { StatusBar } from 'expo-status-bar';
-// import { View } from 'react-native';
+import { View } from 'react-native';
 // import { SafeAreaProvider } from 'react-native-safe-area-context';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen'
 
 export default function App() {
 
   return (
-    <WelcomeScreen />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}  
+    >
+      <View style={{
+        backgroundColor: 'lightblue',
+        width: 100,
+        height: 100,
+        borderWidth: 10,
+        borderColor: 'royalblue',
+        borderRadius: 10,
+      }}></View>
+    </View>
   )
 }
 
