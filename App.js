4ba54ed4d272073f04fd78d8b0593f07cd97619f
Cam MacDonald <camm@rocktechcomputer.com>
2022-06-13 16:03:34 -0400

    card component

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen'
 
-import AppButton from './app/components/AppButton';
+import Card from './app/components/Card'
 
 export default function App() {
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}  
-    >
-      <AppButton
-        title="Login"
-        handlePress={() => console.log('Tapped')}
+    <View style={{
+      backgroundColor: '#f8f4f4',
+      padding: 20,
+      paddingTop: 100
+    }}>
+      <Card
+        title='Red jacket for sale!'
+        subTitle='$100'
+        image={require('./app/assets/logo-red.png')}
       />
     </View>
   )
