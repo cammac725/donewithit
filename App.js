418f9d61e82595c87805043233c6e270f617470f
cammac <camm@rocktechcomputer.com>
2022-06-17 14:21:56 -0400

    message screen style

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import { View } from 'react-native';
 
 import Card from './app/components/Card'
 // import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
-import ViewImageScreen from './app/screens/ViewImageScreen'
+// import ViewImageScreen from './app/screens/ViewImageScreen'
+import MessagesScreen from './app/screens/MessagesScreen'
+
 
 export default function App() {
 
   return (
-    <ViewImageScreen />
+    <MessagesScreen />
+
+    // <ViewImageScreen />
 
     // <ListingDetailsScreen />
 
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,27 +1,51 @@
 import React from 'react';
-import { FlatList } from 'react-native'
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  StatusBar } from 'react-native'
+import Constants from 'expo-constants'
 
-const message = [
+import ListItem from '../components/ListItem'
+
+const messages = [
   {
     id: 1,
     title: 'T1',
-    description:: 'D1',
+    description: 'D1',
     image: require('../assets/mosh.jpg')
   },
   {
     id: 2,
     title: 'T2',
-    description:: 'D2',
+    description: 'D2',
     image: require('../assets/mosh.jpg')
   }
 ]
 
 function MessagesScreen(props) {
   return (
-    <FlatList
-      data={messages}
-    />
+    <SafeAreaView style={styles.screen}>
+      <FlatList
+        data={messages}
+        keyExtractor={message => message.id.toString()}
+        renderItem={({ item }) =>
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        }
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight
+  }
+})
 
 export default MessagesScreen;
